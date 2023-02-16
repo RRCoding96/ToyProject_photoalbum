009d88b89c4f5f4d9d722f6eec8199b2e9e239c4
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-02-16 23:34:16 +0900

    test: 등록되지 않은 이름으로 앨범을 조회했을 때 예외 발생 확인하는 test 작성

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -2,11 +2,16 @@ package com.squarecross.photoalbum.service;
 
 import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -27,5 +32,17 @@ class AlbumServiceTest {
 
         Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
         assertEquals("테스트", resAlbum.getAlbumName());
+    }
+
+    @Test
+    @DisplayName("이름으로 앨범 찾기 실패시 예외 발생")
+    void getAlbumByNameException() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            Album unSavedName = albumService.getAlbumByName("없는 이름");
+        });
     }
 }
