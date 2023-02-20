6b450d285e5125da4b7253432a73b2420c1a6ec4
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-02-20 23:23:17 +0900

    test: AlbumDto에서 Count값이 잘 출력되는지 확인하는 test 작성

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -1,8 +1,10 @@
 package com.squarecross.photoalbum.service;
 
 import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.repository.AlbumRepository;
+import com.squarecross.photoalbum.repository.PhotoRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,7 +23,8 @@ class AlbumServiceTest {
 
     @Autowired
     AlbumRepository albumRepository;
-
+    @Autowired
+    PhotoRepository photoRepository;
     @Autowired
     AlbumService albumService;
 
@@ -45,5 +48,32 @@ class AlbumServiceTest {
         assertThrows(EntityNotFoundException.class, () -> {
             Album unSavedName = albumService.getAlbumByName("없는 이름");
         });
+    }
+
+    @Test
+    void testPhotoCount() {
+        Album album = new Album();
+        album.setAlbumName("테스트 앨범");
+        Album savedAlbum = albumRepository.save(album);
+
+        // 사진을 생성하고, setAlbum을통해 앨범을 지정해준 이후, repository에 사진을 저장한다
+        Photo photo1 = new Photo();
+        photo1.setFileName("사진1");
+        photo1.setAlbum(savedAlbum);
+        photoRepository.save(photo1);
+
+        Photo photo2 = new Photo();
+        photo2.setFileName("사진2");
+        photo2.setAlbum(savedAlbum);
+        photoRepository.save(photo2);
+
+        Photo photo3 = new Photo();
+        photo3.setFileName("사진3");
+        photo3.setAlbum(savedAlbum);
+        photoRepository.save(photo3);
+
+        int expectedCount = 3;
+        int actualCount = photoRepository.countByAlbum_AlbumId(savedAlbum.getAlbumId());
+        assertThat(actualCount).isEqualTo(expectedCount);
     }
 }
