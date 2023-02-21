0e216419dae0a528a2029337146aebe90461a231
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-02-21 23:21:53 +0900

    feat: AlbumController -> query에 albumId를 보내 Album 가져오는 기능 구현

--- src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -5,10 +5,7 @@ import com.squarecross.photoalbum.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/albums")
@@ -22,5 +19,12 @@ public class AlbumController {
         AlbumDto album = albumService.getAlbum(albumId);
         return new ResponseEntity<>(album, HttpStatus.OK);
     }
+
+    @GetMapping("/query")
+    public ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam long albumId) {
+        AlbumDto album = albumService.getAlbum(albumId);
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
 
 }
