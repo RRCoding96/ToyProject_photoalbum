6402ab83484964690dc0cfeeea3d87e3e3d0af8f
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-02-26 00:07:17 +0900

    test: 앨범명 변경 test 작성

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -130,4 +130,20 @@ class AlbumServiceTest {
         assertEquals("aaab", resName.get(1).getAlbumName()); // 1번째 Index가 두번째 앨범명 aaab 인지 체크
         assertEquals(2, resName.size()); // aaa 이름을 가진 다른 앨범이 없다는 가정하에, 검색 키워드에 해당하는 앨범 필터링 체크
     }
+
+    @Test
+    void testChangedAlbumName() throws IOException {
+        AlbumDto albumDto = new AlbumDto();
+        albumDto.setAlbumName("변경전");
+        AlbumDto res = albumService.createAlbum(albumDto);
+
+        Long albumId = res.getAlbumId();
+        AlbumDto updateDto = new AlbumDto();
+        updateDto.setAlbumName("변경후");
+        albumService.changeName(albumId, updateDto);
+
+        AlbumDto updatedDto = albumService.getAlbum(albumId);
+
+        assertEquals("변경후", updatedDto.getAlbumName());
+    }
 }
