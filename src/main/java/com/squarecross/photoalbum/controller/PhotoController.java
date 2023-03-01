988168919cafccf0dd132311e6ec651f1807e1f8
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-03-01 21:02:45 +0900

    feat: 사진 업로드 하는 기능 구현

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/albums/{albumId}/photos")
@@ -18,5 +23,16 @@ public class PhotoController {
     public ResponseEntity<PhotoDto> getPhotoInfo(@PathVariable("photoId") final Long photoId) {
         PhotoDto photoDto = photoService.getPhoto(photoId);
         return new ResponseEntity<>(photoDto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<List<PhotoDto>> uploadPhotos(@PathVariable("albumId") final Long albumId,
+                                                       @RequestParam("photos") MultipartFile[] files) throws IOException {
+        List<PhotoDto> photoDtos = new ArrayList<>();
+        for (MultipartFile file : files) {
+            PhotoDto photoDto = photoService.savePhoto(file, albumId);
+            photoDtos.add(photoDto);
+        }
+        return new ResponseEntity<>(photoDtos, HttpStatus.OK);
     }
 }
